8ff4c9604dc709c9637ce727704ab76859e87578
Mateiu Cristian <102381148+MateiuCristian@users.noreply.github.com>
2026-03-05 21:04:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-function drawPiece(piecePos) {
-    let canvasElem = document.getElementById("myCanvas"), canvasCtx = canvasElem.getContext('2d');
-    window.devicePixelRatio = 2;
-    canvasElem.width *= window.devicePixelRatio;
-    canvasElem.height *= window.devicePixelRatio;
-    canvasCtx.beginPath();
-    console.log(piecePos.x, piecePos.y);
-    canvasCtx.arc(100, 100, 20, 0, Math.PI * 2);
-    console.log(canvasCtx);
-    canvasCtx.stroke();
-
-   // canvasCtx.stroke();
-}
+colorTurn = ["red", "yellow"]
 
 function placePiece(cellId) {
-    let cellPos = document.getElementById(cellId).getBoundingClientRect();
-    let piecePos = cellPos;
-    drawPiece(piecePos);
+    let finalPos = 0;
+    let id = setInterval(function() {
+        document.getElementById("bt " + finalPos.toString() + " " + cellId[5]).setAttribute('style', 'background-color:' + colorTurn[0] + ' !important;');
+        console.log(document.getElementById("bt " + finalPos.toString() + " " + cellId[5]).style.backgroundColor);
+        ++finalPos;
+        if (finalPos == 4) {
+            clearInterval(id);
+        }
+    }, 75);
+    swap
 }
